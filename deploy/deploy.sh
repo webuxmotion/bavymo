8ddb50a8663167875c6c0b9a9661b9138cd7d52f
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-08-21 17:13:38 +0300

    working file

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,7 +7,7 @@ APP_NAME="bavymo"
 APP_DIR="/var/www/$APP_NAME"
 REPO_URL="https://github.com/webuxmotion/bavymo"
 DOMAIN=""
-VITE_SOCKET_URL=""
+VITE_SOCKET_URL="https://$DOMAIN"
 EMAIL=""
 BASE_URL="https://$DOMAIN"
 
@@ -55,9 +55,10 @@ server {
 }
 EOL
 
-sudo ln -sf $NGINX_TEMP_CONF /etc/nginx/sites-enabled/
+sudo ln -sf $NGINX_TEMP_CONF /etc/nginx/sites-enabled/$APP_NAME-temp
 sudo nginx -t
-sudo systemctl reload nginx
+sudo systemctl stop nginx
+sudo systemctl start nginx
 
 # ==============================
 # OBTAIN SSL CERTIFICATES
@@ -71,41 +72,41 @@ sudo certbot certonly --webroot -w /var/www/certbot \
 NGINX_CONF="/etc/nginx/sites-available/$APP_NAME"
 sudo tee $NGINX_CONF > /dev/null <<EOL
 server {
-    listen 80;
-    server_name $DOMAIN www.$DOMAIN;
-    return 301 https://$host$request_uri;
-}
-
-server {
-    listen 443 ssl;
-    server_name $DOMAIN www.$DOMAIN;
-
-    ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;
-
-    location /webhook {
-        proxy_pass http://localhost:9000/webhook;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host $host;
-        proxy_cache_bypass $http_upgrade;
+        listen 80;
+        server_name $DOMAIN www.$DOMAIN;
+        return 301 https://$host$request_uri;
     }
 
-    location / {
-        proxy_pass http://localhost:4000;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host $host;
-        proxy_cache_bypass $http_upgrade;
+    server {
+        listen 443 ssl;
+        server_name $DOMAIN www.$DOMAIN;
+
+        ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem;
+        ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;
+
+        location /webhook {
+            proxy_pass http://localhost:9000/webhook;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade \$http_upgrade;
+            proxy_set_header Connection "upgrade";
+            proxy_set_header Host \$host;
+            proxy_cache_bypass \$http_upgrade;
+        }
+
+        location / {
+            proxy_pass http://localhost:4000;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade \$http_upgrade;
+            proxy_set_header Connection "upgrade";
+            proxy_set_header Host \$host;
+            proxy_cache_bypass \$http_upgrade;
+        }
     }
-}
 EOL
 
 sudo ln -sf $NGINX_CONF /etc/nginx/sites-enabled/
-sudo nginx -t -c /etc/nginx/sites-enabled/$APP_NAME
-sudo nginx -c /etc/nginx/sites-enabled/$APP_NAME
+sudo nginx -t
+sudo systemctl restart nginx
 
 # ==============================
 # APP DEPLOYMENT
