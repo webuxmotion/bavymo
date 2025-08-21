bf8f595244611271a06534314f6f19e9b3b884bc
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-08-21 16:04:55 +0300

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -86,10 +86,10 @@ server {
     location /webhook {
         proxy_pass http://localhost:9000/webhook;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection 'upgrade';
-        proxy_set_header Host $host;
-        proxy_cache_bypass $http_upgrade;
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
     }
 
     location / {
