2bb5b9289d6c452ceef86538c38e9bdf2af88773
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-08-23 16:28:25 +0300

    use path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": new URL("./src", import.meta.url).pathname,
-      "@server": new URL("../server/src", import.meta.url).pathname
+      "@": path.resolve(__dirname, "src"),
+      "@server": path.resolve(__dirname, "../server/src"),
     },
   },
 })
