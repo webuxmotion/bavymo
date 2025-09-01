5e8f9430b3545fc0a00753f84a2440ba5bee4413
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-09-02 01:19:12 +0300

    about

--- client/src/pages/About/About.tsx
+++ client/src/pages/About/About.tsx
@@ -1,15 +1,60 @@
 import { useEffect, useRef } from 'react';
+import { createNoise2D } from 'simplex-noise';
 import styles from './About.module.scss';
+
+// -----------------------------
+// Types
+// -----------------------------
+type Point = {
+  x: number;
+  y: number;
+  _x: number;
+  _y: number;
+};
+
+type Line = Point[];
+
+let lines: Line[] = [];
+
+let linesNumber = 4;
+let vertices = 100;
+let color = "rgba(180, 212, 255, 1)";
+let radius = 200;
+let time = 0;
+
+for (let i = 0; i < linesNumber; i++) {
+  lines[i] = [];
+
+  for (let j = 0; j <= vertices; j++) {
+    let point = {
+      x: Math.cos(j / vertices * Math.PI * 2),
+      y: Math.sin(j / vertices * Math.PI * 2),
+      _x: 0,
+      _y: 0,
+    };
+
+    point._x = point.x;
+    point._y = point.y;
+
+    lines[i].push(point);
+  }
+}
+
 
 function About() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
+    const noise2D = createNoise2D();
+
     const canvas = canvasRef.current;
     if (!canvas) return; // ← Guard clause: stop if canvas is null
 
     const ctx = canvas.getContext('2d');
     if (!ctx) return; // ← Guard clause: stop if context failed
+
+    const halfX = window.innerWidth / 2;
+    const halfY = window.innerHeight / 2;
 
     const circle = {
       x: 50,
@@ -17,7 +62,7 @@ function About() {
       dx: 2,
       dy: 2,
       radius: 30,
-      color: 'purple',
+      color,
     };
 
     const resizeCanvas = () => {
@@ -37,6 +82,32 @@ function About() {
       ctx.closePath();
     };
 
+    let noise;
+
+    const updateLines = () => {
+      lines.forEach((_, idx) => {
+        for (let i = 0; i <= vertices; i++) {
+          noise = noise2D(lines[idx][i]._x / 2 + time * 0.002, lines[idx][i]._y / 2 + time * 0.002);
+
+          lines[idx][i].x = lines[idx][i]._x * radius * (1 - idx / 12) + noise * radius / 5;
+          lines[idx][i].y = lines[idx][i]._y * radius * (1 - idx / 6) + noise * radius / 5;
+        }
+      });
+    }
+
+    const drawLines = () => {
+      lines.forEach(line => {
+        for (let i = 1; i <= vertices; i++) {
+          ctx.beginPath();
+
+          ctx.moveTo(halfX + line[i - 1].x, halfY + line[i - 1].y);
+          ctx.lineTo(halfX + line[i].x, halfY + line[i].y);
+
+          ctx.stroke();
+        }
+      });
+    }
+
     const updateCircle = () => {
       circle.x += circle.dx;
       circle.y += circle.dy;
@@ -51,8 +122,12 @@ function About() {
 
     const animate = () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.strokeStyle = color;
+      updateLines();
+      drawLines();
       drawCircle();
       updateCircle();
+      time++;
       requestAnimationFrame(animate);
     };
 
